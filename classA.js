d4d86f41c8f6015b7e08d31ea70da739b25b97b0
Cristian.Reinoso@mymedzed.com <cristian.reinoso@oktana.com>
2022-01-14 14:53:41 -0500

    Creating new class

---
+++ classA.js
@@ -0,0 +1,20 @@
+function function1() {
+    console.log('print function1');
+}
+
+function function2() {
+    console.log('print function 2');
+    console.log('second commit');
+}
+
+function function3() {
+    console.log('function3');
+}
+
+function function4() {
+    console.log('function4');
+}
+
+function function5() {
+    console.log('function5');
+}
